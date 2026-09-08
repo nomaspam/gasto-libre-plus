c806b4305ea63b639fbb9224a395880639140bac
Gasto Libre <gasto-libre@local>
2026-09-08 12:48:53 +0200

    Actualiza la caché de la aplicación

--- gasto-libre-sw.js
+++ gasto-libre-sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'gasto-libre-';
-const CACHE = `${CACHE_PREFIX}v5`;
+const CACHE = `${CACHE_PREFIX}v6`;
 const APP_SHELL = './';
 const ASSETS = [APP_SHELL, './gasto-libre-manifest.json', './gasto-libre-icon.svg'];
 
